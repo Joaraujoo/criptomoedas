c1c8c13a475bc452e66c6c1e02be956107803b80
Joao Araujo <163578593+Joaraujoo@users.noreply.github.com>
2025-04-24 18:24:16 -0300

    penultima parte

--- src/pages/home/index.tsx
+++ src/pages/home/index.tsx
@@ -2,12 +2,78 @@ import style from "./home.module.css"
 import { BsSearch } from "react-icons/bs"
 import { Link, useNavigate } from "react-router-dom"
 
-import { useState, FormEvent } from "react"
+import { useState, FormEvent, useEffect } from "react"
+import axios from "axios"
+
+interface CoinProps{
+  id: string;
+  name: string;
+  symbol: string;
+  priceUsd: string;
+  vwap24Hr: string;
+  changePercent24Hr: string;
+  rank: string;
+  supply: string;
+  maxSupply: string;
+  marketCapUsd: string;
+  volumeUsd24Hr: string;
+  explorer: string;
+  formatedPrice?: string;
+  formatedMarket?: string;
+  formatedVolume?: string;
+}
+
+interface DataProp{
+  data: CoinProps[];
+}
 
 export function Home(){
 
   const [input, setInput] = useState("")
+  const [coins, setcoins] = useState<CoinProps[]>([])
+
   const navigate = useNavigate()
+
+  useEffect(() => {
+    getData()
+  }, [])
+
+  //Requiziçao da API
+  async function getData(){
+    try{
+      const response = await axios<DataProp>("https://rest.coincap.io/v3/assets?limit=10&offset=0&apiKey=a3ee552b1ffab98352f813e9d2525021ec4df9266072a552600a585320a550a5")
+    
+      const coinsData = response.data.data
+
+      //Formata a moeda (Intl é uma biblioteca internacional)
+      const price = Intl.NumberFormat("en-US", {
+        style: "currency",
+        currency: "USD"
+      })
+
+      const priceCompact = Intl.NumberFormat("en-US", {
+        style: "currency",
+        currency: "USD",
+        notation: "compact"
+      })
+
+      //Adiciona as moedas formatadas no array da API
+      const formatedResult = coinsData.map((item) => {
+        const formated = {
+          ...item,
+          formatedPrice: price.format(Number(item.priceUsd)),
+          formatedMarket: priceCompact.format(Number(item.marketCapUsd)),
+          formatedVolume: priceCompact.format(Number(item.volumeUsd24Hr))
+        }
+        return formated
+      })
+      setcoins(formatedResult)
+      
+    } catch(erro){
+        console.log("Erro ao buscar dados de moedas:", erro)
+    }
+   
+  }
 
   //FUNÇAO PARA PESQUISAR A MOEDA
   function handleSubmit(e: FormEvent){
@@ -54,33 +120,37 @@ export function Home(){
 
             <tbody id="tbody">
 
-              <tr className={style.tr}>
+              {coins.length > 0 && coins.map((item) => (
+                <tr className={style.tr} key={item.id}>
 
                 <td className={style.tdLabel} data-label="Moeda">
+                  
                   <div className={style.name}>
-                     <Link to={"/detail/bitcoin"}>
-                       <span>Bitcoin</span> | BTC
+                  <img className={style.logo} src={`https://assets.coincap.io/assets/icons/${item.symbol.toLowerCase()}@2x.png`} alt="Logo" />
+                     <Link to={`/detail/${item.id}`}>
+                       <span>{item.name}</span> | {item.symbol}
                      </Link>
                   </div>
                 </td>
 
                 <td className={style.tdLabel} data-label="Valor mercado">
-                  1T
+                  {item.formatedMarket}
                 </td>
 
                 <td className={style.tdLabel} data-label="Preço">
-                  8.000
+                  {item.formatedPrice}
                 </td>
 
                 <td className={style.tdLabel} data-label="Volume">
-                  2B
+                  {item.formatedVolume}
                 </td>
 
-                <td className={style.tdProfit} data-label="Mudança 24h">
-                  <span>1.20</span>
+                <td className={Number(item.changePercent24Hr) > 0 ? style.tdProfit : style.tdLoss} data-label="Mudança 24h">
+                  <span>{Number(item.changePercent24Hr).toFixed(3)}</span>
                 </td>
                 
               </tr>
+              ))}
 
             </tbody>
           </table>
